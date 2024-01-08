8384a9470bd3b1391365772374b82d9252eab72b
Rahul Sanap <rahulsanap@Rahuls-MacBook-Air.local>
2024-01-08 16:10:34 -0500

    Final

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const port = 8000; // Define your desired port
 
 // Middleware to parse JSON requests
 app.use(express.json(),cors());
+
 const uri = process.env.MONGODB_URL; // Replace with your MongoDB URI and database name
 
 //connecting MongoDB
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
- const db=mongoose.connection;
+const db=mongoose.connection;
 
 db.once('open',()=>console.log("Connected"))
 
@@ -21,6 +22,20 @@ db.once('open',()=>console.log("Connected"))
 app.get('/', (req, res) => {
   res.send('Hello, this is your Express app!');
 });
+
+
+//User Route
+const userRoute=require('./routes/userRoute');
+app.use('/user',userRoute,cors());
+
+//Post Route
+const postRoute=require('./routes/postRoute');
+app.use('/post',postRoute,cors());
+
+//Comment Route
+const commentRoute=require('./routes/commentRoute');
+app.use('/comment',commentRoute,cors());
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
